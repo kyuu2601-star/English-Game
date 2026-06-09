3c2f858b9979901f8c849a89e3ff9b94e5b62427
kyuu2601-star <kyuu2601@gmail.com>
2026-06-09 13:08:56 +0700

    Add manifest.js for Mon English Game

---
+++ manifest.js
@@ -0,0 +1,16 @@
+{
+  "short_name": "MonEnglish",
+  "name": "Mon English Game",
+  "icons": [
+    {
+      "src": "assets/Game_Logo.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ],
+  "start_url": "index.html",
+  "background_color": "#020617",
+  "theme_color": "#020617",
+  "display": "standalone",
+  "orientation": "landscape"
+}
