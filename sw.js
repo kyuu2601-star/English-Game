51e4633b517c16efc22955d1134d1a34e88c749a
kyuu2601-star <kyuu2601@gmail.com>
2026-06-09 13:14:52 +0700

    Refactor cached assets in service worker
    Updated cached assets in service worker for various screens, removing unused assets and adding necessary files.

--- sw.js
+++ sw.js
@@ -5,12 +5,14 @@ const ASSETS_TO_CACHE = [
   './global.css',
   './core-engine.js',
   './manifest.json',
+  
+  // Cache cứng font chữ và các asset nền tảng nằm ở folder assets gốc
   './assets/Fredoka.ttf',
   './assets/PatrickHand.ttf',
   './assets/Game_Logo.png',
   './assets/Background_Loading.png',
   
-  // Cache cứng bộ khung giao diện UI và CSS của các màn hình
+  // Cache các file code chạy module của các màn hình
   './screens/loading-menu/ui.html',
   './screens/loading-menu/style.css',
   './screens/loading-menu/logic.js',
@@ -18,44 +20,14 @@ const ASSETS_TO_CACHE = [
   './screens/main-menu/ui.html',
   './screens/main-menu/style.css',
   './screens/main-menu/logic.js',
-  './screens/main-menu/assets/Btn_Start.png',
-  './screens/main-menu/assets/Btn_Start_Pressed.png',
-  './screens/main-menu/assets/Btn_Collection.png',
-  './screens/main-menu/assets/Btn_Collection_Pressed.png',
-  './screens/main-menu/assets/Btn_Backpack_Icon.png',
-  './screens/main-menu/assets/Btn_Backpack_Icon_Pressed.png',
-  './screens/main-menu/assets/Btn_Setting_Icon.png',
-  './screens/main-menu/assets/Btn_Setting_Icon_Pressed.png',
-  './screens/main-menu/assets/Nametag_lv5.png',
-  './screens/main-menu/assets/UI_Answer_Box.png',
-  './screens/main-menu/assets/UI_Answer_Box_Pressed.png',
   
   './screens/battle-stage/ui.html',
   './screens/battle-stage/style.css',
   './screens/battle-stage/logic.js',
-  './screens/battle-stage/assets/BG_Desert.png',
-  './screens/battle-stage/assets/BG_Forest.png',
-  './screens/battle-stage/assets/BG_Snow.png',
-  './screens/battle-stage/assets/BG_Volcano.png',
-  './screens/battle-stage/assets/UI_Question_Box.png',
-  './screens/battle-stage/assets/UI_Answer_Box.png',
-  './screens/battle-stage/assets/UI_Answer_Box_Pressed.png',
-  './screens/battle-stage/assets/VFX_Ball_Open.png',
-  './screens/battle-stage/assets/VFX_Ball_Close.png',
-  './screens/battle-stage/assets/VFX_Smoke.png',
-  './screens/battle-stage/assets/VFX_Star_Shiny.png',
-  './screens/battle-stage/assets/Popup_Captured.png',
-  './screens/battle-stage/assets/Popup_Missed.png',
   
   './screens/collection-book/ui.html',
-  './screens/collection-book/assets/Background_Collection.png',
   './screens/collection-book/style.css',
-  './screens/collection-book/logic.js',
-  './screens/collection-book/assets/Tag_Icon.png',
-  './screens/collection-book/assets/Btn_Back.png',
-  './screens/collection-book/assets/Btn_Back_Pressed.png',
-  './screens/collection-book/assets/Btn_Battle_Icon.png',
-  './screens/collection-book/assets/Btn_Battle_Icon_Pressed.png'
+  './screens/collection-book/logic.js'
 ];
 
 // Cài đặt Service Worker và ép nạp toàn bộ danh sách asset tĩnh vào Cache Storage
